b663cdd35809d8d8d501b5e4b9aab4f95c30c85b
Prasad, Sawantdesai <prasad.sawantdesai@knorr-bremse.com>
2020-07-08 00:25:33 +0530

    cleaned up database and implemented changes related to data type and collection methods

--- database/commservice.db.sql
+++ database/commservice.db.sql
@@ -1,61 +1,68 @@
 BEGIN TRANSACTION;
+DROP TABLE IF EXISTS "tag_group_mapping";
+CREATE TABLE IF NOT EXISTS "tag_group_mapping" (
+	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	"tag_group_id"	INTEGER NOT NULL,
+	"tag_id"	INTEGER NOT NULL,
+	FOREIGN KEY("tag_id") REFERENCES "tags"("id"),
+	FOREIGN KEY("tag_group_id") REFERENCES "taggroups"("id")
+);
+DROP TABLE IF EXISTS "taggroups";
+CREATE TABLE IF NOT EXISTS "taggroups" (
+	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	"name"	TEXT NOT NULL UNIQUE,
+	"plcid"	INTEGER,
+	"machineid"	INTEGER,
+	"collection_method"	INTEGER,
+	"collection_type"	TEXT,
+	FOREIGN KEY("machineid") REFERENCES "machine"("id"),
+	FOREIGN KEY("plcid") REFERENCES "controller"("id")
+);
+DROP TABLE IF EXISTS "tags";
 CREATE TABLE IF NOT EXISTS "tags" (
-	"id"	INTEGER PRIMARY KEY AUTOINCREMENT,
-	"name"	TEXT,
+	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	"name"	TEXT NOT NULL UNIQUE,
 	"register_typeid"	INTEGER,
 	"address"	INTEGER,
 	"scaling"	INTEGER,
 	"data_type"	INTEGER,
 	"data_size"	INTEGER,
-	"tag_groupid"	INTEGER,
-	FOREIGN KEY("register_typeid") REFERENCES "variables"("id")
+	FOREIGN KEY("register_typeid") REFERENCES "register_types"("id")
 );
-CREATE TABLE IF NOT EXISTS "taggroups" (
-	"id"	TEXT UNIQUE,
-	"plcid"	INTEGER,
-	"machineid"	INTEGER,
-	"collection_method"	TEXT,
-	"collection_type"	TEXT,
-	PRIMARY KEY("id"),
-	FOREIGN KEY("machineid") REFERENCES "machine"("id"),
-	FOREIGN KEY("plcid") REFERENCES "controller"("id")
+DROP TABLE IF EXISTS "register_types";
+CREATE TABLE IF NOT EXISTS "register_types" (
+	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	"ragister_type"	TEXT,
+	"range_upper"	INTEGER,
+	"range_lower"	INTEGER
 );
-CREATE TABLE IF NOT EXISTS "variables" (
-	"id"	INTEGER PRIMARY KEY AUTOINCREMENT,
-	"name"	TEXT,
-	"range_start"	INTEGER,
-	"range_lower"	INTEGER,
-	"plcid"	INTEGER,
-	FOREIGN KEY("plcid") REFERENCES "controller"("id")
-);
+DROP TABLE IF EXISTS "machine";
 CREATE TABLE IF NOT EXISTS "machine" (
-	"id"	INTEGER PRIMARY KEY AUTOINCREMENT,
+	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
 	"name"	TEXT,
 	"manualid"	INTEGER,
 	"plcid"	INTEGER,
 	FOREIGN KEY("plcid") REFERENCES "controller"("id")
 );
+DROP TABLE IF EXISTS "driver";
+CREATE TABLE IF NOT EXISTS "driver" (
+	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	"name"	TEXT NOT NULL UNIQUE,
+	"format"	TEXT
+);
+DROP TABLE IF EXISTS "connection";
 CREATE TABLE IF NOT EXISTS "connection" (
-	"id"	INTEGER PRIMARY KEY AUTOINCREMENT,
-	"connection_string"	TEXT,
+	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	"connection_string"	TEXT NOT NULL,
 	"driverid"	INTEGER,
 	"plcid"	INTEGER,
 	FOREIGN KEY("driverid") REFERENCES "driver"("id"),
 	FOREIGN KEY("plcid") REFERENCES "controller"("id")
 );
-CREATE TABLE IF NOT EXISTS "driver" (
-	"id"	INTEGER PRIMARY KEY AUTOINCREMENT,
-	"name"	TEXT UNIQUE,
-	"format"	TEXT
-);
+DROP TABLE IF EXISTS "controller";
 CREATE TABLE IF NOT EXISTS "controller" (
-	"id"	INTEGER PRIMARY KEY AUTOINCREMENT,
-	"name"	TEXT UNIQUE,
+	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+	"name"	TEXT NOT NULL UNIQUE,
 	"description"	TEXT
 );
-INSERT INTO "driver" VALUES (1,'Modbus TCP','IPADDRESS;PORT');
-INSERT INTO "driver" VALUES (2,'Modbus RTU','DEVICE_PORT;BAUDRATE;PARITY;DATA_BITS;STOP_BITS');
-INSERT INTO "controller" VALUES (4,'plc1','details');
-INSERT INTO "controller" VALUES (5,'plc2','details');
-INSERT INTO "controller" VALUES (6,'plc3','details');
 COMMIT;
