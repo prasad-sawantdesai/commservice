b9d2d69d6c9d7dcb2e384bf9e1a04044059175c6
Prasad, Sawantdesai <prasad.sawantdesai@knorr-bremse.com>
2020-08-08 10:39:00 +0530

    updated install script and build

--- gui/installer_build_script.bat
+++ gui/installer_build_script.bat
@@ -1,6 +1,7 @@
 pyinstaller.exe --onefile -i resources\\app.ico --name x-boardConfigurator --distpath bin main.py
 xcopy resources bin\resources\ /E /H /K
 xcopy configs bin\configs\ /E /H /K
+xcopy database bin\database\ /E /H /K
 rmdir build /s /q
 del x-boardConfigurator.spec
 
