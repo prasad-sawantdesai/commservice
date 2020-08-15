21daa6b4e2f173f560e2d474be1547ddf5c44054
Prasad, Sawantdesai <prasad.sawantdesai@knorr-bremse.com>
2020-08-15 17:22:32 +0530

    updated installation files

--- gui/x-boardconfigurator.sh
+++ gui/x-boardconfigurator.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # run GUI
+# Provide path of virtual environment and activate command
 source /home/designx/prasad/python3_virtualenv/commservice_env/bin/activate
+# Change directory to gui folder of commservice
 cd /home/designx/prasad/git_repo_current/commservice/gui
 ./main.py
 
