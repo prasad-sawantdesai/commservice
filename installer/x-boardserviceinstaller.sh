e9c22774fe9471ae3448e8397a4b1cd4a6e13a73
prasad-sawantdesai <prasad.sawantdesai@gmail.com>
2020-08-25 21:59:49 +0530

    updated

--- installer/x-boardserviceinstaller.sh
+++ installer/x-boardserviceinstaller.sh
@@ -27,4 +27,5 @@ mkdir python3_virtualenv
 cd python3_virtualenv/
 virtualenv -p /usr/bin/python3 commservice_env
 source /home/x-board/python3_virtualenv/commservice_env/bin/activate
+cd /home/x-board/installer
 pip install -r requirements.txt
